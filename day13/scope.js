c7a266a5da76032529985f26d1cb7ae6f1e514f7
moonJung <use770@naver.com>
2021-04-13 15:49:44 +0900

    스코프

--- day13/scope.js
+++ day13/scope.js
@@ -62,8 +62,65 @@ if (exec[3]) {
         console.log(x);
     }
 
-    foo();
+    foo();          // local
 
-    console.log(x);
+    console.log(x); // global
     
+}
+
+
+if (exec[4]) {
+
+    // 전역 함수
+    function foo() {
+        console.log('global function foo');
+    }
+
+    function bar() {
+        // 중첩 함수
+        function foo() {
+            console.log('local function foo');
+        }
+
+        foo();
+    }
+
+    bar();  // local function foo
+
+}
+
+
+if (exec[5]) {
+    
+    var x = 1;
+
+    if (true) {
+        // var 키워드로 선언된 변수는 함수의 코드 블록(함수 몸체)만을 지역 스코프로 인정한다.
+        // 함수 밖에서 var 키워드로 선언된 변수는 코드 블록 내에서 선언되었다 할지라도 모두 전역 변수다.
+        // 따라서 x는 전역 변수다. 이미 선언된 전역 변수 x가 있으므로 x 변수는 중복 선언된다.
+        // 이는 의도치 않게 변수 값이 변경되는 부작용을 발생시킨다.
+        var x = 10;
+    }
+
+    console.log(x);     // 10
+
+}
+
+
+if (exec[6]) {
+
+    var x = 1;
+
+    function foo() {
+        var x = 10;
+        bar();
+    }
+
+    function bar() {
+        console.log(x);
+    }
+
+    foo();  // 1
+    bar();  // 1
+
 }
